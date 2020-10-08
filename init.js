0469fb3157874f2a2cd229505f4f84fb67bd38d5
Federico Gon <federico.gon@internetbrands.com>
2020-10-08 12:43:28 -0300

    Increase timeout

--- init.js
+++ init.js
@@ -2,16 +2,20 @@
 console.log('== INIT Upwork extension.');
 
 function loadDefaultValues() {
-    Array.from(document.querySelectorAll('div[name=timeFrom] ul.eo-dropdown-menu li a:not(.active)')).find(el => el.textContent === '9:00am').click();
-    Array.from(document.querySelectorAll('div[name=timeTo] ul.eo-dropdown-menu li a:not(.active)')).find(el => el.textContent === '5:00pm').click();
+    console.log('++ Setting default Values...');
+    Array.from(document.querySelectorAll('div[name=timeFrom] ul.eo-dropdown-menu li a:not(.active)')).find(el => el.textContent === '09:00').click();
+    Array.from(document.querySelectorAll('div[name=timeTo] ul.eo-dropdown-menu li a:not(.active)')).find(el => el.textContent === '17:00').click();
     let memo = document.querySelector('textarea[name=memo].ng-pristine');
     memo.value = 'REGULAR';
     memo.dispatchEvent(new Event('change'));
 }
 
-document.addEventListener('click', function (e) {
-    if ((e.target.tagName === 'SPAN' && e.target.classList.contains('air-icon-add'))) {
-        console.log('++ Timeout for loadDefaultValues');
-        setTimeout(loadDefaultValues, 3000);
-    }
-}, false);
+setTimeout(() => {
+    console.log('++ Listening...');
+    document.addEventListener('click', function (e) {
+        if ((e.target.tagName === 'SPAN' && e.target.classList.contains('air-icon-add'))) {
+            console.log('++ Timeout for loadDefaultValues');
+            setTimeout(loadDefaultValues, 5000);
+        }
+    }, false);
+}, 3000);
