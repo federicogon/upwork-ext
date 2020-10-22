288d46ff649ed12c95aa50af4a015e5c58d6984c
Federico Gon <federico.gon@internetbrands.com>
2020-10-22 18:22:22 -0300

    Added watchers to manual time modal

--- init.js
+++ init.js
@@ -1,21 +1,58 @@
 'use strict';
 console.log('== INIT Upwork extension.');
 
-function loadDefaultValues() {
-    console.log('++ Setting default Values...');
-    Array.from(document.querySelectorAll('div[name=timeFrom] ul.eo-dropdown-menu li a:not(.active)')).find(el => el.textContent === '09:00').click();
-    Array.from(document.querySelectorAll('div[name=timeTo] ul.eo-dropdown-menu li a:not(.active)')).find(el => el.textContent === '17:00').click();
-    let memo = document.querySelector('textarea[name=memo].ng-pristine');
-    memo.value = 'REGULAR';
-    memo.dispatchEvent(new Event('change'));
+
+function selectTime(name, time) {
+    let dropdowns = document.querySelectorAll('div[name=' + name + '] ul.eo-dropdown-menu li a:not(.active)');
+    if (dropdowns.length) {
+        let item = Array.from(dropdowns).find(el => el.textContent === time);
+        if (item) item.click();
+    }
 }
 
-setTimeout(() => {
-    console.log('++ Listening...');
-    document.addEventListener('click', function (e) {
-        if ((e.target.tagName === 'SPAN' && e.target.classList.contains('air-icon-add'))) {
-            console.log('++ Timeout for loadDefaultValues');
-            setTimeout(loadDefaultValues, 5000);
-        }
-    }, false);
-}, 3000);
+function loadDefaultValues() {
+    console.log('++ Setting default Values...');
+    selectTime('timeFrom', '09:00');
+    selectTime('timeTo', '17:00');
+    document.querySelectorAll('textarea[name=memo]').forEach(memo => {
+        memo.value = 'REGULAR';
+        memo.dispatchEvent(new Event('change'));
+    });
+}
+
+function addDefaultValuesLink() {
+    let modalTitle = document.querySelectorAll('div[slot="header-title"]');
+    if (modalTitle.length) {
+        modalTitle.forEach(el => {
+            let a = document.createElement('a');
+            a.innerText = 'Load Default Values';
+            a.href = '#';
+            a.onclick = (e) => {
+                e.preventDefault();
+                loadDefaultValues();
+            }
+            el.appendChild(a);
+        });
+    } else {
+        setTimeout(addDefaultValuesLink, 100);
+    }
+}
+
+function watchModal() {
+    console.log('++ Waiting for MODAL...');
+    if (document.querySelectorAll('div[slot="header-title"]').length) {
+        loadDefaultValues();
+        addDefaultValuesLink();
+        return;
+    }
+    setTimeout(watchModal, 100);
+}
+
+function isAddManualTimeButton(el) {
+    return (el.target.tagName === 'SPAN' && el.target.classList.contains('air-icon-add')) || el.target.classList.contains('add-manual-time-button')
+}
+
+console.log('++ Listening...');
+document.addEventListener('click', (e) => {
+    if (isAddManualTimeButton(e)) watchModal();
+}, false);
